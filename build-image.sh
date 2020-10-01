56aeb5c9c71675e94fe40a12f7a585618c8b5a80
pabloheigvd <pabloheigvd@gmail.com>
2020-10-02 01:31:59 +0200

    run locally et run image marche

--- build-image.sh
+++ build-image.sh
@@ -4,15 +4,4 @@
 echo "--------------------------------------------"
 echo "--- Maven build of the StackOverflow app..."
 echo "--------------------------------------------"
-mvn package
-
-FILE=./target/stack.war
-if test -f "$FILE"; then
-  echo "$FILE exists."
-
-  echo "--------------------------------------------"
-  echo "--- Moving .war file in docker folder..."
-  echo "--------------------------------------------"
-  mv $FILE ./docker/images/payara/stack.war &&
-  echo "done... Please follow the instructions to build the Docker image"
-fi
+mvn clean package
