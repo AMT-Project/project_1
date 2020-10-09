4a80cae949fa13cc6d8ba5259f67f1b4c9dffab6
Kleeer <delhomme.claire.ts7@gmail.com>
2020-10-09 10:25:37 +0200

    Fix #10 Refactor name classes to match maven testclass pattern "Test###"

--- src/test/java/TestPerson.java
+++ src/test/java/TestPerson.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UTPerson {
+public class TestPerson {
 
     private static final String USER = "wasadigi";
     private static final String FIRSTNAME = "Olivier";
