ef0c4f85c94f0da5b711884afa78ae96522228c4
pabloheigvd <pabloheigvd@gmail.com>
2020-10-20 09:17:36 +0200

    script for launching easily

---
+++ mysql/initDB.sql
@@ -0,0 +1,11 @@
+DROP DATABASE amt;
+
+CREATE DATABASE amt;
+
+USE amt;
+
+CREATE TABLE persons(
+	id INT,
+    name varchar(50)
+);
+
---
+++ runDocker.sh
@@ -0,0 +1,7 @@
+echo "================================="
+echo "=== Build & launch test image ==="
+echo "================================="
+build-image.sh
+
+docker-compose up
+
