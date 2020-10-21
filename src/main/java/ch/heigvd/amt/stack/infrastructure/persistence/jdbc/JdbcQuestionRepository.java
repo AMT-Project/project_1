27d41ca0118b1db7466e44f35bde46b47fa448fe
pabloheigvd <pabloheigvd@gmail.com>
2020-10-21 17:26:09 +0200

    more passed tests

--- src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/jdbc/JdbcQuestionRepository.java
+++ src/main/java/ch/heigvd/amt/stack/infrastructure/persistence/jdbc/JdbcQuestionRepository.java
@@ -17,6 +17,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -34,9 +35,26 @@ public class JdbcQuestionRepository extends JdbcRepository<Question, QuestionId>
     @Override
     public Collection<Question> find(QuestionsQuery query) {
         //  TODO implement
+        //   pas d'utilisation de query????
         try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement("");
-            ResultSet rs = super.getSet(preparedStatement);
+            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(
+                    "SELECT * FROM Question");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            LinkedList<Question> questions = new LinkedList<>();
+
+            while(rs.next()){
+                Question question = Question.builder()
+                        .id(new QuestionId(rs.getString("uuid")))
+                        .title(rs.getString("title"))
+                        .description(rs.getString("description"))
+                        .authorUUID(new PersonId(rs.getString("person_uuid")))
+                        .build();
+                questions.add(question);
+            }
+
+            return questions;
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -44,11 +62,17 @@ public class JdbcQuestionRepository extends JdbcRepository<Question, QuestionId>
     }
 
     @Override
-    public void save(Question entity) throws SQLIntegrityConstraintViolationException {
+    public void save(Question question) throws SQLIntegrityConstraintViolationException {
         //  TODO implement
         try {
-            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement("");
-
+            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(
+                    "INSERT INTO Question (uuid, title, description, person_uuid)" +
+                    "VALUES (?,?,?,?)");
+            preparedStatement.setString(1, question.getId().asString());
+            preparedStatement.setString(2, question.getTitle());
+            preparedStatement.setString(3, question.getDescription());
+            preparedStatement.setString(4, question.getAuthorUUID().asString());
+            preparedStatement.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -57,20 +81,72 @@ public class JdbcQuestionRepository extends JdbcRepository<Question, QuestionId>
     @Override
     public void remove(QuestionId id) {
         //  TODO implement
-
+        try {
+            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(
+                    "DELETE FROM Question * WHERE id=?");
+            preparedStatement.setString(1, id.asString());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public Optional<Question> findById(QuestionId id) {
         //  TODO implement
+        try {
+            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(
+                    "SELECT * FROM Question WHERE id=?");
+            preparedStatement.setString(1, id.asString());
+            ResultSet rs = preparedStatement.executeQuery();
+
+            LinkedList<Question> questions = new LinkedList<>();
+
+            while(rs.next()){
+                Question question = Question.builder()
+                        .id(new QuestionId(rs.getString("uuid")))
+                        .title(rs.getString("title"))
+                        .description(rs.getString("description"))
+                        .authorUUID(new PersonId(rs.getString("person_uuid")))
+                        .build();
+                questions.add(question);
+            }
+
+            if(questions.size() != 1) {
+                return Optional.empty();
+            }
+
+            return Optional.of(questions.get(0));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
     public Collection<Question> findAll() {
         //  TODO implement
+        try {
+            PreparedStatement preparedStatement = dataSource.getConnection().prepareStatement(
+                    "SELECT * FROM Question");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            LinkedList<Question> questions = new LinkedList<>();
+
+            while(rs.next()){
+                Question question = Question.builder()
+                        .id(new QuestionId(rs.getString("uuid")))
+                        .title(rs.getString("title"))
+                        .description(rs.getString("description"))
+                        .authorUUID(new PersonId(rs.getString("person_uuid")))
+                        .build();
+                questions.add(question);
+            }
+
+            return questions;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
-
-
 }
