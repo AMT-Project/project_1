ad3fcb5f220e3da4075ea15b0d234ab13ada1450
pabloheigvd <pabloheigvd@gmail.com>
2020-10-23 11:48:35 +0200

    see my question on my profile page + no I wait since useless

--- e2e/6profile_test.js
+++ e2e/6profile_test.js
@@ -2,10 +2,12 @@ Feature("Profile");
 
 const stackURL = "http://stack.ch:9080/stack";
 const profileURL = "/profile";
+const loginPage = stackURL + "/login";
 
 const uniqueId = new Date().getTime();
 const firstName = "Codecept";
 const lastName = "JS";
+const userFullName = firstName + " " + lastName;
 const uniqueUsername = "6profile_test-" + uniqueId;
 const uniqueEmail = "sixprofile@" + uniqueId + ".ch";
 const pwd = "pwd";
@@ -14,7 +16,6 @@ Scenario("after registering, I can see my own profile page", (I) => {
     I.amOnPage(stackURL + "/register");
     I.register(uniqueUsername, firstName, lastName, uniqueEmail, pwd);
 
-    const userFullName = firstName + " " + lastName;
     I.click(userFullName);
 
     I.amOnPage(stackURL + profileURL);
@@ -28,4 +29,21 @@ Scenario("after registering, I can see my own profile page", (I) => {
     I.see("Asked questions list");
     I.see(userFullName + "'s " + "profile")
     I.see("0")
+});
+
+Scenario("After posting a question, I see my question on my profile page", (I) => {
+    I.amOnPage(loginPage);
+    I.login(uniqueUsername, pwd);
+
+    I.click("New Question");
+    const questionTitle = "How to browse stack.ch?"
+    I.fillField("title", questionTitle);
+    const questionDescription = "I am really lost there, any help?"
+    I.fillField("description", questionDescription);
+    I.click("Submit");
+
+    I.click(userFullName);
+
+    I.see(questionTitle.toUpperCase());
+    I.see(questionDescription);
 });
