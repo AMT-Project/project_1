4f8fc7ec8d6c06cb728aae9ad998bb52c6820ef2
pabloheigvd <pabloheigvd@gmail.com>
2020-10-24 21:22:50 +0200

    cleanup SeContainer code

--- src/test/java/ch/heigvd/amt/stack/application/identitymgmt/IdentityManagementFacadeTest.java
+++ src/test/java/ch/heigvd/amt/stack/application/identitymgmt/IdentityManagementFacadeTest.java
@@ -4,28 +4,17 @@ import ch.heigvd.amt.stack.application.ServiceRegistry;
 import ch.heigvd.amt.stack.application.identitymgmt.login.RegisterCommand;
 import ch.heigvd.amt.stack.domain.person.PersonId;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import javax.enterprise.inject.se.SeContainer;
-import javax.enterprise.inject.se.SeContainerInitializer;
-import javax.inject.Inject;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 class IdentityManagementFacadeTest {
 
-    static SeContainerInitializer initializer;
-
-    @BeforeAll
-    public static void setupCdi(){
-        initializer = SeContainerInitializer.newInstance();
-    }
-
     // test exception: https://howtodoinjava.com/junit5/expected-exception-example/
     @Test
     void registerThrowsExceptionWhenDuplicateUserWithSameUsername() {
-        try(SeContainer container = initializer.initialize()){
+        try{
             ServiceRegistry serviceRegistry = (ServiceRegistry)
                     new InitialContext().lookup("ServiceRegistry");
             RegisterCommand registerCommand = RegisterCommand.builder()
@@ -46,7 +35,6 @@ class IdentityManagementFacadeTest {
                     .build();
             IdentityManagementFacade identityManagementFacade = serviceRegistry
                     .getIdentityManagementFacade();
-            // FIXME datasource is null in the personRepository of the facade
             identityManagementFacade.register(registerCommand);
             Assertions.assertThrows(RegistrationFailedException.class, () -> {
                 identityManagementFacade.register(registerCommandDuplicate);
