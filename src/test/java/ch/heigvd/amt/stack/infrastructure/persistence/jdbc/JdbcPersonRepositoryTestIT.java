f2fc3eb981e4052f2da39279ed06ab133ef77b31
pabloheigvd <pabloheigvd@gmail.com>
2020-10-28 23:06:52 +0100

    added count tests

--- src/test/java/ch/heigvd/amt/stack/infrastructure/persistence/jdbc/JdbcPersonRepositoryTestIT.java
+++ src/test/java/ch/heigvd/amt/stack/infrastructure/persistence/jdbc/JdbcPersonRepositoryTestIT.java
@@ -3,14 +3,28 @@ package ch.heigvd.amt.stack.infrastructure.persistence.jdbc;
 import ch.heigvd.amt.stack.domain.person.Person;
 import ch.heigvd.amt.stack.domain.person.PersonId;
 import ch.heigvd.amt.stack.infrastructure.persistence.jdbc.helper.DataSourceProvider;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class JdbcPersonRepositoryTestIT {
 
     static JdbcPersonRepository repository;
+    static int i = 0;
+
+    public static Person getPerson(){
+        i++;
+        Person person = Person.builder()
+                .username("toto" + i)
+                .lastName("Dupont")
+                .firstName("Dupont")
+                .clearTextPassword("1234")
+                .email("toto" + i + "@example.com").build();
+        return person;
+    }
 
     @BeforeAll
     public static void setup(){
@@ -21,5 +35,21 @@ class JdbcPersonRepositoryTestIT {
     public void findByIdReturnsNullWhenNoPersonIsFound(){
         Person nonExistingPerson = repository.findById(new PersonId()).orElse(null);
         assertNull(nonExistingPerson);
+    }
+
+    @Test
+    public void addingPersonIncreasesCount(){
+        int n = repository.count();
+        repository.save(getPerson());
+        assertEquals(n + 1, repository.count());
+    }
+
+    @Test
+    public void removingPersonDecreasesCount(){
+        Person p = getPerson();
+        repository.save(p);
+        int n = repository.count();
+        repository.remove(p.getUuid());
+        assertEquals(n - 1, repository.count());
     }
 }
