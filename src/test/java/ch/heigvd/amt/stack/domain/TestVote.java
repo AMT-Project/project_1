da2e6c5344caa88ad244952efa9713a7e7766755
Kleeer <delhomme.claire.ts7@gmail.com>
2020-10-29 17:15:55 +0100

    vote

--- src/test/java/ch/heigvd/amt/stack/domain/TestVote.java
+++ src/test/java/ch/heigvd/amt/stack/domain/TestVote.java
@@ -1,4 +1,57 @@
 package ch.heigvd.amt.stack.domain;
 
+import ch.heigvd.amt.stack.domain.person.PersonId;
+import ch.heigvd.amt.stack.domain.question.vote.Vote;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
 public class TestVote {
+    @Mock
+    private static PersonId pid;
+
+    @BeforeAll
+    private static void brief(){
+        pid = mock(PersonId.class);
+    }
+
+    @Test
+    public void validCreateVote(){
+        Vote vote = Vote.builder()
+                .authorUUID(pid)
+                .build();
+    }
+
+    @Test
+    public void invalidCreateVote(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Vote vote = Vote.builder()
+                    //.authorUUID(pid)
+                    .build();
+        });
+
+    }
+
+    @Test
+    public void deepCloning(){
+        Vote vote = Vote.builder()
+                .authorUUID(pid)
+                .build();
+
+        Vote clone = vote.deepClone();
+
+        assertEquals(vote.getAnswerUUID(), clone.getAnswerUUID());
+        assertEquals(vote.getAuthorUUID(), clone.getAuthorUUID());
+        assertEquals(vote.getQuestionUUID(), clone.getQuestionUUID());
+        assertEquals(vote.isUpvote(), clone.isUpvote());
+
+        assertNotEquals(vote.getUuid(), clone.getUuid());
+    }
+
 }
