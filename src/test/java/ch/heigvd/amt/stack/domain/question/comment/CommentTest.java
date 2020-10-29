e676ee40ee8f3325ff41480cdc74274f210d1f9a
pabloheigvd <pabloheigvd@gmail.com>
2020-10-29 14:45:51 +0100

    unit test no author for comment

---
+++ src/test/java/ch/heigvd/amt/stack/domain/question/comment/CommentTest.java
@@ -0,0 +1,33 @@
+package ch.heigvd.amt.stack.domain.question.comment;
+
+import ch.heigvd.amt.stack.domain.person.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CommentTest {
+
+    @Test
+    public void throwsIllegalArgumentExceptionWhenNoContent(){
+        Person p = Person.builder()
+                .username("ti")
+                .firstName("OIjo")
+                .lastName("wqeuq")
+                .email("ti@example.com")
+                .clearTextPassword("pwd")
+                .build();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Comment.builder()
+                    .authorUUID(p.getUuid())
+                    .build();
+        });
+    }
+
+    @Test
+    public void throwIllegalArgumentExceptionWhenNoAuthor(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Comment.builder()
+                    .content("hi")
+                    .build();
+        });
+    }
+}
